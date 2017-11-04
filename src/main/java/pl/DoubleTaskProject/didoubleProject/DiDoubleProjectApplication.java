da837727ba8ababf4f5bc68d549ad01aefe44d12
Mazgul <mateusz.mazgaj15@gmail.com>
2017-11-04 13:41:53 +0100

    Initial commit

---
+++ src/main/java/pl/DoubleTaskProject/didoubleProject/DiDoubleProjectApplication.java
@@ -0,0 +1,12 @@
+package pl.DoubleTaskProject.didoubleProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiDoubleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiDoubleProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/DoubleTaskProject/didoubleProject/DiDoubleProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.DoubleTaskProject.didoubleProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiDoubleProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
